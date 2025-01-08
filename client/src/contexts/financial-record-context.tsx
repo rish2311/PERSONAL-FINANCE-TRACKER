9d4f814ddf0ecc3c54d5bf6efa07fa5a84a0952f
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-01-08 19:38:52 +0530

    fixed the client side issues

--- client/src/contexts/financial-record-context.tsx
+++ client/src/contexts/financial-record-context.tsx
@@ -1,8 +1,8 @@
 import { useUser } from "@clerk/clerk-react";
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export interface FinancialRecord {
-  id?: string;
+  _id?: string;
   userId: string;
   date: Date;
   description: string;
@@ -13,10 +13,12 @@ export interface FinancialRecord {
 
 interface FinancialRecordsContextType {
   records: FinancialRecord[];
-  addRecord: (record: FinancialRecord) => Promise<void>;
+  addRecord: (record: FinancialRecord) => void;
+  updateRecord: (id: string, newRecord: FinancialRecord) => void;
+  deleteRecord: (id: string) => void;
 }
 
-const FinancialRecordsContext = createContext<
+export const FinancialRecordsContext = createContext<
   FinancialRecordsContextType | undefined
 >(undefined);
 
@@ -28,53 +30,99 @@ export const FinancialRecordsProvider = ({
   const [records, setRecords] = useState<FinancialRecord[]>([]);
   const { user } = useUser();
 
-  const fetchRecords = async() => {
+  const fetchRecords = async () => {
     if (!user) return;
     const response = await fetch(
-      `http://localhost:3001/financial-records/getAllByUserID/${user?.id ?? ""}`
+      `http://localhost:3001/financial-records/getAllByUserID/${user.id}`
     );
 
-    if (response.ok){
+    if (response.ok) {
       const records = await response.json();
+      console.log(records);
       setRecords(records);
     }
   };
 
   useEffect(() => {
     fetchRecords();
-  }, []);
+  }, [user]);
 
   const addRecord = async (record: FinancialRecord) => {
+    const response = await fetch("http://localhost:3001/financial-records", {
+      method: "POST",
+      body: JSON.stringify(record),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
     try {
-      const response = await fetch("http://localhost:3001/financial-records", {
-        method: "POST",
-        body: JSON.stringify(record),
+      if (response.ok) {
+        const newRecord = await response.json();
+        setRecords((prev) => [...prev, newRecord]);
+      }
+    } catch (err) {}
+  };
+
+  const updateRecord = async (id: string, newRecord: FinancialRecord) => {
+    const response = await fetch(
+      `http://localhost:3001/financial-records/${id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(newRecord),
         headers: {
           "Content-Type": "application/json",
         },
-      });
+      }
+    );
 
+    try {
       if (response.ok) {
         const newRecord = await response.json();
-        newRecord.date = new Date(newRecord.date);
-        setRecords((prev) => [...prev, newRecord]);
-      } else {
-        console.error("Failed to add record:", response.statusText);
+        setRecords((prev) =>
+          prev.map((record) => {
+            if (record._id === id) {
+              return newRecord;
+            } else {
+              return record;
+            }
+          })
+        );
       }
-    } catch (err) {
-      console.error("Error while adding record:", err);
-    }
+    } catch (err) {}
+  };
+
+  const deleteRecord = async (id: string) => {
+    const response = await fetch(
+      `http://localhost:3001/financial-records/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    try {
+      if (response.ok) {
+        const deletedRecord = await response.json();
+        setRecords((prev) =>
+          prev.filter((record) => record._id !== deletedRecord._id)
+        );
+      }
+    } catch (err) {}
   };
 
   return (
-    <FinancialRecordsContext.Provider value={{ records, addRecord }}>
+    <FinancialRecordsContext.Provider
+      value={{ records, addRecord, updateRecord, deleteRecord }}
+    >
       {children}
     </FinancialRecordsContext.Provider>
   );
 };
 
 export const useFinancialRecords = () => {
-  const context = useContext(FinancialRecordsContext);
+  const context = useContext<FinancialRecordsContextType | undefined>(
+    FinancialRecordsContext
+  );
 
   if (!context) {
     throw new Error(
--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,16 +1,22 @@
-import {SignedIn,SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/clerk-react";
+import { Navigate } from "react-router-dom";
 
 export const Auth = () => {
   return (
-    <div className="sign-in container">
+    <div className="sign-in-container">
       <SignedOut>
-        <SignUpButton mode="modal"/>
-        <SignInButton mode="modal"/>
+        <h1> Welcome to Your Own Personal Finance Tracker!</h1>
+        <SignUpButton mode="modal" />
+        <SignInButton mode="modal" />
       </SignedOut>
       <SignedIn>
-        <UserButton />
+        <Navigate to="/" />
       </SignedIn>
     </div>
   );
 };
-
